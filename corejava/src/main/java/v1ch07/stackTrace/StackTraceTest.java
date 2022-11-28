bfb212a0bc3ec6297cefc9d6c73440ae01d25150
滕广银 <1766707503@qq.com>
2022-11-28 14:57:32 +0800

    core java

---
+++ corejava/src/main/java/v1ch07/stackTrace/StackTraceTest.java
@@ -0,0 +1,29 @@
+package v1ch07.stackTrace;
+
+import java.util.Scanner;
+
+/**
+ * @author 滕广银
+ * @description TODO
+ * @date 2022/11/28 14:14
+ */
+public class StackTraceTest {
+    public static int factorial(int n) {
+        System.out.println("factorial(" + n + "):");
+        var walker = StackWalker.getInstance();
+        walker.forEach(System.out::println);
+        int r;
+        if (n <= 1) r = 1;
+        else r = n * factorial(n - 1);
+        System.out.println("return" + r);
+        return r;
+    }
+
+    public static void main(String[] args) {
+        try (var in = new Scanner(System.in)) {
+            System.out.print("Enter n: ");
+            int n = in.nextInt();
+            factorial(n);
+        }
+    }
+}
